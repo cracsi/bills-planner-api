00c6a59e3b05d7bef0f998ed0364d61e3382b0fa
cracsi <98497136+cracsi@users.noreply.github.com>
2026-07-11 10:49:11 -0500

    correción de mapeo de ruta "facturas", creación de colección de postman con pruebas iniciales

--- src/facturas/facturas.module.ts
+++ src/facturas/facturas.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FacturasService } from './facturas.service';
+import { FacturasController } from './facturas.controller';
+import { Factura } from './entities/factura.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Factura])],
+  controllers: [FacturasController],
+  providers: [FacturasService],
+})
 export class FacturasModule {}
