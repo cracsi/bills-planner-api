418fa11ce3dcee5c5b0310416dbeb86a974c1d7a
cracsi <98497136+cracsi@users.noreply.github.com>
2026-07-10 22:37:32 -0500

    auth coompleto, build correcto

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsuariosModule } from '../usuarios/usuarios.module';
 
 @Module({
+  imports: [
+    UsuariosModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+  secret: configService.get<string>('JWT_SECRET'),
+  signOptions: {
+    expiresIn: (configService.get<string>('JWT_EXPIRES_IN') ?? '1d') as any,
+  },
+}),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
